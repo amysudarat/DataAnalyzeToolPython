20de08ddf0438fba8c10bfa8962b432633940ab0
Amy <amy.sudarat@gmail.com>
2018-08-14 23:44:39 -0400

    show IAPS number along with description and fix the aspect ratio between axes

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ from importData import filterIAPS
 
 # import Data
 # declare file path (put r in front to convert normal string to raw string)
-IAPs_file_path = r"C:\Users\DSPLab\Research\IAPSdata\AllSubjects_1-20.txt"
+# IAPs_file_path = r"C:\Users\DSPLab\Research\IAPSdata\AllSubjects_1-20.txt"
+IAPs_file_path = r"C:\Research\AllSubjects_1-20.txt"
 IAPS_df = importIAPS(IAPs_file_path)
 
 # get list of indexs by pin point location (df,valence,arousal,width)
-indexOfDescription = filterIAPS(IAPS_df,5.5,3.5,width=0.2)
+indexOfDescription = filterIAPS(IAPS_df,9,9,width=0.2)
 # Select rows by list of index
 IAPS_filtered_df = IAPS_df.loc[indexOfDescription,:]
 print(IAPS_filtered_df)
