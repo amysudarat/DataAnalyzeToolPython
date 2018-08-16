bdae2e70d96d1f4916790e408e2a0dab5c57e5af
Amy <amy.sudarat@gmail.com>
2018-08-15 22:02:42 -0400

    add calculateCoordinateFromAngle function and extraMath library

--- extraMath.py
+++ extraMath.py
@@ -1,11 +1,16 @@
 import math
 
-def calculateCoordinateFromAngle(coorX,angle):
+def calculateCoordinateFromAngle(r,angle):
     '''
-    return the coordinate of y from angle(in degrees)
+    r= radius, angle= angle in degrees
+    return the coordinate of x,y from angle(in degrees) 
+    circumplex's origin is translated from (0,0) to (4.5,4.5)
+    the coordinate of x,y in unit circle that turn seta angle
+    (r*cos@ +a),(r*sin@+b)
     '''
-    r = abs(coorX-4.5)
-    coorY = r*math.sin(math.radians(angle))
-    coorY = float(format(coorY,'.2f'))+5
-    return coorY
+    coorX = r*math.cos(math.radians(angle))+4.5
+    coorX = float(format(coorX,'.2f'))
+    coorY = r*math.sin(math.radians(angle))+4.5
+    coorY = float(format(coorX,'.2f'))
+    return (coorX, coorY)
     
--- testConsole.py
+++ testConsole.py
@@ -1,7 +1,5 @@
 from extraMath import calculateCoordinateFromAngle
 
 # set x coordination
-x = 6.5
-y = calculateCoordinateFromAngle(x,90)
-y = float(format(y,'.2f'))
-print(y)
+r = 2
+print(calculateCoordinateFromAngle(r,30))
